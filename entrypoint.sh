1369a9ddb9fc44fa5c07a9dc39a582746dad8043
Stephan Sundermann <stephansundermann@gmail.com>
2019-03-07 09:57:02 +0100

    Add versioning

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,8 @@ cd /opt/autominy/catkin_ws
 source /opt/ros/melodic/setup.bash
 rosdep install --from-paths . --ignore-src --rosdistro=melodic --simulate | awk 'NF>1{print $NF}' | sed '1d' | paste -sd "," - > DEPS
 DEPENDENCIES=$(cat DEPS)
+VERSION=$(git rev-parse --short HEAD)
+BUILDATE=$(date +"%Y%m%d%H%M")
 rosdep install --from-paths . --ignore-src --rosdistro=melodic -y
 catkin build
 
@@ -15,6 +17,7 @@ cd /opt
 git clone https://github.com/autominy/debian-packaging.git
 cd debian-packaging
 sed -i -e "s/DEPS/$DEPENDENCIES/g" autominy-basic-packages/DEBIAN/control
+sed -i -e "s/DATE/$BUILDATE-$VERSION/g" autominy-basic-packages/DEBIAN/control
 cp -r /opt/autominy autominy-basic-packages/opt/ 
 dpkg-deb --build autominy-basic-packages
 
