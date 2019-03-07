3db9df8c664f461fb2bf1ebc0744da51dda1a4d5
Stephan Sundermann <stephansundermann@gmail.com>
2019-03-07 10:02:41 +0100

    Fix typo

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ source /opt/ros/melodic/setup.bash
 rosdep install --from-paths . --ignore-src --rosdistro=melodic --simulate | awk 'NF>1{print $NF}' | sed '1d' | paste -sd "," - > DEPS
 DEPENDENCIES=$(cat DEPS)
 VERSION=$(git rev-parse --short HEAD)
-BUILDATE=$(date +"%Y%m%d%H%M")
+BUILDDATE=$(date +"%Y%m%d%H%M")
 rosdep install --from-paths . --ignore-src --rosdistro=melodic -y
 catkin build
 
@@ -17,7 +17,7 @@ cd /opt
 git clone https://github.com/autominy/debian-packaging.git
 cd debian-packaging
 sed -i -e "s/DEPS/$DEPENDENCIES/g" autominy-basic-packages/DEBIAN/control
-sed -i -e "s/DATE/$BUILDATE-$VERSION/g" autominy-basic-packages/DEBIAN/control
+sed -i -e "s/DATE/$BUILDDATE-$VERSION/g" autominy-basic-packages/DEBIAN/control
 cp -r /opt/autominy autominy-basic-packages/opt/ 
 dpkg-deb --build autominy-basic-packages
 
